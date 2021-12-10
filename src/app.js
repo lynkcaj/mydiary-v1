fbca414aeedc1c7370bdebdcc36b2f20273d0b56
Jackielyn Benitez <jackielyn.benitez@accenture.com>
2021-12-10 12:46:13 +0800

    first commit

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ const path = require('path');
 
 const app = express()
 
+const PORT = process.env.PORT || 8080;
+
 app.use(express.json())
 app.use(bodyParser.json())
 app.use(express.static('public'))
